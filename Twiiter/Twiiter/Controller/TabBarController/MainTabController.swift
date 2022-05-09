a22eda82129d45f40db102ac2aca6feacb721d89
Roy-wonji <shuwj81@daum.net>
2022-05-09 16:48:46 +0900

    ✨[feat]: 메인뷰에 메뉴 버튼 추가 구현#1

--- Twiiter/Twiiter/Controller/TabBarController/MainTabController.swift
+++ Twiiter/Twiiter/Controller/TabBarController/MainTabController.swift
@@ -6,30 +6,40 @@
 //
 
 import UIKit
+import Then
 
 final class MainTabController: UITabBarController {
     //MARK: - Properties
-    
+    lazy var actionButton = UIButton(type: .system).then { button in
+        button.tintColor = .white
+        button.backgroundColor = .twitterBlue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(handleActionButtonTapped), for: .touchUpInside)
+    }
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewControllers()
-        
+        updateUI()
     }
     
+    //MARK: - Selectors
+    @objc func handleActionButtonTapped() {
+        print("DEBUG: actionbutton tapped ...")
+    }
     //MARK:  - UI관련
     private func configureViewControllers() {
-        let feed = tempateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"), rootViewController: FeedController())
-        let explore = tempateNavigationController(unselectedImage: UIImage(named: "search_unselected"), selectedImage: UIImage(named: "search_selected"), rootViewController: ExploreController())
+        let feed = tempateNavigationController(unselectedImage: UIImage(named: "home_unselected"), selectedImage: UIImage(named: "home_selected"),  rootViewController: FeedController())
+        let explore = tempateNavigationController(unselectedImage: UIImage(named: "search_unselected"), selectedImage: UIImage(named: "search_selected"),  rootViewController: ExploreController())
         let notifications = tempateNavigationController(unselectedImage: UIImage(named: "like_unselected"), selectedImage: UIImage(named: "like_selected"), rootViewController: NotificationController())
-        let conversations = tempateNavigationController(unselectedImage: UIImage(named: "mail_unselected"), selectedImage: UIImage(named: "mail_selected"), rootViewController: ConversationsController())
+        let conversations = tempateNavigationController(unselectedImage: UIImage(named: "mail_unselected"), selectedImage: UIImage(named: "mail_selected"),  rootViewController: ConversationsController())
         
         viewControllers = [feed, explore, notifications, conversations]
         tabBar.tintColor = .black
         tabBar.backgroundColor = .white
         tabBar.barTintColor = .purple
         self.overrideUserInterfaceStyle =  .light
-        
     }
     //MARK: - tabbar 의 이미지가 선택 되었을때랑 안선택 되었을때 이미지 선택 해주는 함수
     func tempateNavigationController(unselectedImage: UIImage?, selectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
@@ -37,5 +47,20 @@ final class MainTabController: UITabBarController {
         navigation.tabBarItem.image = unselectedImage
         navigation.tabBarItem.selectedImage = selectedImage
         return navigation
+    }
+    
+    private func updateUI() {
+        configureUI()
+    }
+    
+    private func configureUI() {
+        setActionButton()
+    }
+    
+    private func setActionButton() {
+        view.addSubview(actionButton)
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
+                            paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius =  56 / 2
     }
 }
